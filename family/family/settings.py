1b8e3a571d5e19575ca44054ca6e316159599c88
Bo Vial <bvial@macquariecloudservices.com>
2020-09-08 16:57:19 +1000

    remove secret key

--- family/family/settings.py
+++ family/family/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^(6u%^ccho6rv-9y6l#w07y84ujaq7tz5)4$z1(u14m&*mbi3^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
